fd6a0c40d89b6a987b7904665dcb868fea011ce9
Thuyen Code <thuyencode@proton.me>
2024-10-02 21:09:52 +0700

    Reduce the font size in the footer

--- src/client/components/layout/root/footer.tsx
+++ src/client/components/layout/root/footer.tsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <footer className='footer footer-center bg-base-300 p-3 text-base-content'>
       <aside>
         <a
-          className='link-hover link inline-flex items-center gap-1 font-semibold'
+          className='link-hover link inline-flex items-center gap-1 text-sm font-semibold'
           href='https://github.com/thuyencode/members-only-the-odin-project'
         >
-          <Icon className='text-xl' icon='mdi:copyright' /> THUYEN CODE
+          <Icon className='text-base' icon='mdi:copyright' /> THUYEN CODE
         </a>
       </aside>
     </footer>
