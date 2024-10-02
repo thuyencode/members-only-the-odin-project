513ecb7000bf0e215876dd6bde772310b76379d8
Thuyen Code <thuyencode@proton.me>
2024-10-02 16:54:09 +0700

    Basic CSS rules

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,11 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      container: {
+        center: true
+      }
+    }
   },
   plugins: [daisyui]
 } satisfies Config
