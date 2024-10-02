f349f6ce6010e8c7409a523455084e15748d61bc
Thuyen Code <thuyencode@proton.me>
2024-10-02 19:39:41 +0700

    Make the sign in sign up pages use the same component

---
+++ src/client/modules/(auth)/sign-in/sign-in.page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from '../components/auth-form'
+
+const SignInPage = () => {
+  return <AuthForm />
+}
+
+export default SignInPage
---
+++ src/client/modules/(auth)/sign-up/sign-up.page.tsx
@@ -0,0 +1,7 @@
+import AuthForm from '../components/auth-form'
+
+const SignUpPage = () => {
+  return <AuthForm action='sign-up' />
+}
+
+export default SignUpPage
