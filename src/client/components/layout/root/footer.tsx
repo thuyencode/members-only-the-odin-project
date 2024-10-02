4c67bc58e59a9ba07c03abb8b17929ffb6795219
Thuyen Code <thuyencode@proton.me>
2024-10-02 16:54:50 +0700

    Fix the link in the footer

--- src/client/components/layout/root/footer.tsx
+++ src/client/components/layout/root/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <aside>
         <a
           className='link-hover link inline-flex items-center gap-1 font-semibold'
-          href='https://github.com/thuyencode'
+          href='https://github.com/thuyencode/members-only-the-odin-project'
         >
           <Icon className='text-xl' icon='mdi:copyright' /> THUYEN CODE
         </a>
