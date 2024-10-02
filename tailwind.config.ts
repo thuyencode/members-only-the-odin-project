32fdff49cb065abd7fbe37b6c3ce0cd8af8c29f1
Thuyen Code <thuyencode@proton.me>
2024-10-02 10:08:06 +0700

    Config Tailwind CSS and DaisyUI

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import daisyui from 'daisyui'
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [daisyui]
+} satisfies Config
