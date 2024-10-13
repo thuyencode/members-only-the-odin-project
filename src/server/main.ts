a9fc6e895f575f23a6181a50708044fcd6f04516
Thuyen Code <thuyencode@proton.me>
2024-10-13 11:20:41 +0700

    Use HelmetJS to harden API endpoints, use Express's `compression` to compress data to clients

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,14 +1,22 @@
-import express from 'express'
+import compression from 'compression'
+import e from 'express'
+import helmet from 'helmet'
 import ViteExpress from 'vite-express'
 import env from './lib/utils/env'
 
-const PORT = env.PORT
+const PORT = env.PORT || 8080
 
-const app = express()
+const app = e()
 
-app.get('/hello', (_, res) => {
-  res.send('Hello Vite + React + TypeScript!')
-})
+app.use(e.json())
+
+app.use(compression())
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+)
 
 ViteExpress.listen(app, PORT, () =>
   console.log(`Server is listening on port ${PORT}...`)
