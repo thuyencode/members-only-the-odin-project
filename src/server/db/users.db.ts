d3a22bf80ec93b994e6bed683133a326ff5354a6
Thuyen Code <thuyencode@proton.me>
2024-10-16 12:31:17 +0700

    Add functions to interact with the database

---
+++ src/server/db/users.db.ts
@@ -0,0 +1,42 @@
+import { User } from '@/shared/types'
+import pool from './pool'
+
+const selectByUsername = async (
+  username: string
+): Promise<User | undefined> => {
+  const { rows } = await pool.query<User>(
+    'SELECT * FROM users WHERE username = $1',
+    [username]
+  )
+
+  return rows[0]
+}
+
+const selectUserById = async (id: number): Promise<User | undefined> => {
+  const { rows } = await pool.query<User>('SELECT * FROM users WHERE id = $1', [
+    id
+  ])
+
+  return rows[0]
+}
+
+const insertUser = async ({
+  name,
+  username,
+  salted_hash
+}: Omit<User, 'id'>): Promise<User> => {
+  const { rows } = await pool.query<User>(
+    'INSERT INTO users (name, username, salted_hash) VALUES ($1::text, $2::text, $3::text) RETURNING *;',
+    [name, username, salted_hash]
+  )
+
+  return rows[0]
+}
+
+const Users = {
+  selectByUsername,
+  selectUserById,
+  insertUser
+}
+
+export default Users
