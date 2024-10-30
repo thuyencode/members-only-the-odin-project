ab95286a176aa13aff59d4f32ff166d44c8b39bd
Thuyen Code <thuyencode@proton.me>
2024-10-30 21:57:27 +0700

    Use `rollup-plugin-visualizer` to visualizer front-end bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react(), visualizer()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
